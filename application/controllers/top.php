7494ba1e30211827a96973900c797ffe4e5d320c
RyoFukai <fukai.develop@gmail.com>
2020-12-04 00:47:06 +0900

    検証

--- application/controllers/top.php
+++ application/controllers/top.php
@@ -4,7 +4,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Top extends MY_Controller {
 	public function index()
 	{
-		$data = [];
+		$data = [
+			'name' => '深井'
+		];
 		$this->smarty->view('top.tpl',$data);
 	}
 }
